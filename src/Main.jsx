65820808faa2e828171a5d7549bf9e53a0995957
gavrilenkoanton <59771981+gavrilenkoanton@users.noreply.github.com>
2020-02-10 15:28:24 +0300

    first commit

---
+++ src/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Main.module.css';
+
+
+function Main() {
+  return (
+      <div className={styles.main}>
+          <div className={styles.container}>
+              <div className={styles.myNameIs}>
+                  тырыпыры
+              </div>
+              <div className={styles.photo}></div>
+
+          </div>
+      </div>
+  );
+}
+
+export default Main;
